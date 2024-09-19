04d8aa01c044d03c46ac8cc8e25d61fc02015fec
Sara Stankiewicz <sarastankiewicz@pc002425.partners.org>
2024-09-19 17:08:03 -0400

    beginning to work on ancestor function

--- R/scratch.R
+++ R/scratch.R
@@ -1,37 +1,76 @@
-#' use owlready2 ontology search facility on term labels
-#' @param oents owlents instance
-#' @param regexp character(1) simple regular expression
-#' @param case_sensitive logical(1) should case be respected in search?
-#' @return A named list: term labels are elements, tags are names of elements.
-#' Will return NULL if nothing is found.
+
+#' construct owlents instance from an owl file
+#' @importFrom reticulate import iterate
+#' @param owlfn character(1) path to valid owl ontology
+#' @return instance of owlents, which is a list with clnames (
+#' a vector of term names in form `[namespace]_[tag]`), allents
+#' (a list with python references to owlready2 entities, that
+#' can be operated on using owlready2.EntityClass methods),
+#' owlfn (filename), iri (IRI), call (record of call producing
+#' the entity.)
 #' @examples
 #' pa = get_ordo_owl_path()
 #' orde = setup_entities(pa)
-#' ol = search_labels(orde, "*Immunog*")
-#' plot(orde, names(ol))
-#' @export
-search_labels_work = function (oents, regexp, case_sensitive=TRUE) 
-{
-  #
-  #search(_use_str_as_loc_str=True, _case_sensitive=True, _bm25=False, **kargs)
-  # 
-  proc = basilisk::basiliskStart(bsklenv)
-  on.exit(basilisk::basiliskStop(proc))
-  basilisk::basiliskRun(proc, function(oents ) {
-  stopifnot(inherits(oents, "owlents"))
+#' orde
+#' ancestors(orde[1000:1001])
+#' labels(orde[1000:1001])
+#' @export 
+setup_entities = function(owlfn) {
   thecall = match.call()
   o2 = reticulate::import("owlready2")
-  ont = o2$get_ontology(oents$owlfn)$load()
-  ans = ont$search(`_case_sensitive`=case_sensitive, label = regexp)
-  lans = reticulate::iterate(ans, force)
-  if (length(lans)<=0) return(NULL)
-  allv = lapply(lans, function(x) x$label[0])
-  alln = lapply(lans, function(x) x$name)
-  names(allv) = unlist(alln)
-  allv},oents=oents)
+  ont = o2$get_ontology(owlfn)$load()
+  cl = ont$classes()
+  clnames = iterate(cl, function(x) x$name) # exhausts cl
+  ents = ont$classes()  # new iterator
+  allents = iterate(ents)
+  ans = list(clnames=clnames, allents=allents, owlfn=owlfn, 
+             iri=ont$base_iri, call=thecall)
+  class(ans) = c("owlents", "list")
+  ans
 }
 
-pa = get_ordo_owl_path()
-orde = setup_entities(pa)
-ol = search_labels_work(orde, "*Immunog*")
-setdiff(orde,ol)
+#' retrieve ancestor 'sets'
+#' @param oe owlents instance
+#' @return a list of sets
+#' @examples
+#' pa = get_ordo_owl_path()
+#' orde = setup_entities(pa)
+#' orde
+#' ancestors(orde[1:5])
+#' labels(orde[1:5])
+#' @export
+ancestors_work = function(pa) {
+  #proc = basilisk::basiliskStart(bsklenv)
+  #on.exit(basilisk::basiliskStop(proc))
+  #basilisk::basiliskRun(proc, function(pa) {
+  o2 = reticulate::import("owlready2")
+  ont = o2$get_ontology(owlfn)$load()
+  cl = ont$classes()
+  clnames = iterate(cl, function(x) x$name) # exhausts cl
+  ents = ont$classes()  # new iterator
+  allents = iterate(ents)
+  oe = list(clnames=clnames, allents=allents, owlfn=owlfn, 
+             iri=ont$base_iri, call=thecall)
+  class(oe) = c("owlents", "list")
+  ans = lapply(oe$allents, o2$EntityClass$ancestors)
+  names(ans) = oe$clnames
+  ans
+}
+
+
+
+
+
+#' obtain list of names of a set of ancestors
+#' @param anclist output of `ancestors`
+#' @note non-entities are removed and names are extracted
+#' @return list of vectors of character()
+#' @examples
+#' pa = get_ordo_owl_path()
+#' orde = setup_entities(pa)
+#' al = ancestors(orde[1001:1002])
+#' ancestors_names(al)
+#' @export
+ancestors_names = function(anclist) {
+  lapply(anclist, .ancestor_element_names)
+}
